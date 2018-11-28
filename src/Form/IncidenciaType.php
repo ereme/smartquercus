21ec6526e86fe691463411db0d8c487ae3889a61
Beatriz <btrizch@gmail.com>
2018-11-28 14:34:40 +0100

    Editando el formularios de editar incidencia

--- src/Form/IncidenciaType.php
+++ src/Form/IncidenciaType.php
@@ -7,19 +7,48 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
 
 class IncidenciaType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('fecha')
-            ->add('latitud')
-            ->add('longitud')
-            ->add('descripcion')
+            ->add('fecha', DateType::class, [
+                'widget' => 'single_text'
+            ])
+            ->add('latitud', null, array(
+                'label' => false,
+                'attr' => array(
+                    'placeholder' => 'Latitud',
+                    'class' => 'form-control'
+                )
+            ))
+            ->add('longitud', null, array(
+                'label' => false,
+                'attr' => array(
+                    'placeholder' => 'Latitud',
+                    'class' => 'form-control'
+                )
+            ))
+            ->add('descripcion', TextareaType::class, array(
+                'label' => false,
+                'attr' => array(
+                  'placeholder' => 'Descripción del inmueble',
+                  'class' => 'form-control'
+                )
+
+            ))
             ->add('estado', ChoiceType::class, array(
                 'choices' => Incidencia::ESTADOS
             ))
+            ->add('save', SubmitType::class, array(
+                'attr' => array('class' => 'btn btn-primary float-right'),
+                'label' => 'Guardar'
+            ))   
         ;
     }
 
