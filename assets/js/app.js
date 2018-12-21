3fd0f1f36fe54b866a250f882a8bda0ecb5c3256
Jesus Muñoz <jesus@iluego.com>
2018-12-21 10:49:33 +0100

    Cambio simple: ruta de /inicio a /

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,5 +11,5 @@ require('../css/app.css');
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 // var $ = require('jquery');
 
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+//console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
  
--- src/Controller/InicioController.php
+++ src/Controller/InicioController.php
@@ -21,9 +21,8 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-/**
- * @Route("/inicio")
- */
+
+
 class InicioController extends AbstractController
 {
     /**
